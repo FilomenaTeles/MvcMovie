4d06ddd05dd0f0e622641a6f91098e1ba189dc60
Filomena Patricia Pires Teles <filomena.teles@msft.cesae.pt>
2023-07-19 10:58:14 +0100

    Personalização das mensassens de erro

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
@@ -8,16 +7,16 @@ public class Movie
 {
     public int Id { get; set; }
 
-    [StringLength(60, MinimumLength = 3)]
-    [Required]
+    [StringLength(60, MinimumLength = 3, ErrorMessage ="Título deve ter no minímo 3 letras e no máximo 60.")]
+    [Required (ErrorMessage ="Campo Título é obrigatório.")]
     public string? Title { get; set; }
 
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
 
-    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-    [Required]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Deve começar um uma letra maiúscula e não são admitidos números nem caracteres especiais.")]
+    [Required(ErrorMessage = "Campo Género é obrigatório.")]
     [StringLength(30)]
     public string? Genre { get; set; }
 
@@ -26,8 +25,8 @@ public class Movie
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 
-    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "Deve começar um uma letra maiúscula e não são admitidos caracteres especiais.")]
     [StringLength(5)]
-    [Required]
+    [Required(ErrorMessage = "Campo Rating é obrigatório.")]
     public string? Rating { get; set; }
 }
